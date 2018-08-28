9c4c76088f7d52a4e15ea3d7d0125e33b0f190a4
nkasprak <nkasprak@cbpp.org>
2018-08-28 13:11:07 -0400

    think I neeed this

--- build.js
+++ build.js
@@ -1,7 +1,9 @@
 const browserify = require("browserify");
 const fs = require("fs");
 var dest = fs.createWriteStream("./gulplib_bundled.js");
-var b = browserify();
-b.external("v8");
+var b = browserify({
+  node:true
+});
+b.external(["v8"]);
 b.add("./gulplib.js");
 b.bundle().pipe(dest);
